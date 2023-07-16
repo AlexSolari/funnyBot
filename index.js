b10fa7d5f6f14c28e773b6a07facd66d93d401f7
Alex Solari <alexsanify@gmail.com>
2023-07-17 02:10:51 +0300

    removed dev commandes

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const Bot = require('./entities/bot');
-const CommandBuilder = require('./helpers/commandBuilder');
 const functionality = require('./functionality/functionality');
-const chz = 65947221;
 
 console.log("Initializing bot...");
 
@@ -10,42 +8,6 @@ const bot = new Bot();
 
 functionality.commands.forEach(cmd => bot.addCommand(cmd));
 functionality.triggers.forEach(cmd => bot.addTrigger(cmd));
-
-const devLoad = new CommandBuilder("Dev.Load")
-    .on(/add (?<name>\w+)/i)
-    .do((api, msg, result) => {
-        if (msg.from.id != chz){
-            return;
-        }
-        
-        const commandName = result.groups.name;
-        console.log(`Loading new command [Reaction.${commandName}]...`);
-
-        const command = require(`./functionality/commands/${commandName}`);
-        bot.addCommand(command);
-
-        console.log(`[${command.name}] added successfully`);
-    })
-    .build();
-
-const devUnload = new CommandBuilder("Dev.Unload")
-    .on(/remove (?<name>\w+)/i)
-    .do((api, msg, result) => {
-        if (msg.from.id != chz){
-            return;
-        }
-        
-        const commandName = `Reaction.${result.groups.name}`;
-        console.log(`Unloading command [${commandName}]...`);
-
-        bot.removeCommand(commandName);
-
-        console.log(`[${commandName}] removed successfully`);
-    })
-    .build();
-
-bot.addCommand(devLoad);
-bot.addCommand(devUnload);
 
 fs.readFile('token.prod', 'utf8', (err, data) => {
     if (err) {
