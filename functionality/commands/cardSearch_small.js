7032cb1a9ec0f9deb0ad05cfbc9420be33eb62ea
Alex Solari <alexsanify@gmail.com>
2023-08-27 23:36:54 +0300

    small search

---
+++ functionality/commands/cardSearch_small.js
@@ -0,0 +1,25 @@
+const CommandBuilder = require('../../helpers/commandBuilder');
+
+module.exports = new CommandBuilder("Reaction.CardSearch_Small")
+    .on(/\[(.+)\]/i)
+    .do(async (ctx) => {
+        function getCardText(card){
+            return `${card.image_uris.normal}`;
+        }
+
+        const response = await fetch(`https://api.scryfall.com/cards/named?fuzzy=${ctx.matchResult[1]}`)
+        const json = await response.text();
+        const data = JSON.parse(json);
+
+        if (data.status == 404)
+            return;
+
+        const cards = data.card_faces 
+            ? data.card_faces
+            : [data];
+        const images = cards.map(x => getCardText(x));
+
+        ctx.reply(images[0]);
+    })
+    .cooldown(0)
+    .build();
