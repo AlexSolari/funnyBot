411d0a5aae9332d52251869a3ce98577ee129ac1
Alex Solari <alexsanify@gmail.com>
2024-09-11 16:23:29 +0300

    registration sorting & datetime

--- functionality/commands/registration.js
+++ functionality/commands/registration.js
@@ -24,10 +24,22 @@ export default new CommandBuilder("Reaction.Registration")
         }
 
         const currentWeek = getCurrentWeek();
+        const options = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+          }
 
         const response = await fetch(`https://api.wlaunch.net/v1/company/7ea091e0-359a-11eb-86df-9f45a44f29bd/branch/7ea10724-359a-11eb-86df-9f45a44f29bd/slot/gt/resource?start=${currentWeek.firstDay}&end=${currentWeek.lastDay}&source=WIDGET&withDiscounts=true&preventBookingEnabled=true`);
         const data = await response.json();
-        const resources = data.slots.map(x => x.date_slots.map(ds => ds.slots.find(dss => dss.gt.service.name.indexOf(serviceName) != -1))).flat(Infinity).filter(x => x);
+        const resources = data.slots
+            .map(x => x.date_slots.map(ds => ({date: new Date(ds.date), slots: ds.slots})))
+            .flat(Infinity)
+            .filter(x => x.slots.length > 0 && x.slots.find(dss => dss.gt.service.name.indexOf(serviceName) != -1))
+            .sort((a,b) => a.date - b.date)
+            .map(({date, slots}) => slots.map(x => {x.date = date.toLocaleDateString("uk-UA", options); return x}))
+            .flat();
 
         if (!resources || resources.length == 0) {
             ctx.replyWithText(`поки нема`);
@@ -35,6 +47,7 @@ export default new CommandBuilder("Reaction.Registration")
         }
 
         const eventInfos = resources.map(x => ({
+            date: x.date,
             name: x.gt.name ?? x.gt.service?.name ?? serviceName,
             id: x.id,
             spaces: x.gt.space,
@@ -46,7 +59,7 @@ export default new CommandBuilder("Reaction.Registration")
             : '';
 
         eventInfos.forEach((x, i) => {
-            text += `[${escapeMarkdown(x.name)}](https://w.wlaunch.net/c/magic_world/events/b/7ea10724-359a-11eb-86df-9f45a44f29bd/e/${x.id}) \\(${x.usedSpaces} уже в резі\\)`;
+            text += `[${escapeMarkdown(x.name)}](https://w.wlaunch.net/c/magic_world/events/b/7ea10724-359a-11eb-86df-9f45a44f29bd/e/${x.id}) \\(${x.usedSpaces} уже в резі\\) відбудеться у ${escapeMarkdown(x.date)}`;
 
             if ((i + 1) < eventInfos.length)
                 text += '\n';
