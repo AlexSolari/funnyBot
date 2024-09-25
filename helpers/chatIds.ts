a2441b9f3780c9a398ed97407588ebab16aa4721
Alex Solari <alexsanify@gmail.com>
2024-09-25 13:07:46 +0300

    removed ids

--- helpers/chatIds.ts
+++
@@ -1,8 +0,0 @@
-export enum ChatId {
-    PioneerChat = -1001572054460,
-    ModernChat = -1001734384223,
-    LvivChat = -1001864573102,
-    SpellSeeker = -1001697109061,
-    StandardChat = -1001654682000,
-    PauperChat = -1001715317650,
-}
