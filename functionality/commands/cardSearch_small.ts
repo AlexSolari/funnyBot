f2577fca7b483ed297eb7e07c0c14c3367100969
Alex Solari <alexsanify@gmail.com>
2024-12-20 11:01:30 +0200

    card search improv

--- functionality/commands/cardSearch_small.ts
+++ functionality/commands/cardSearch_small.ts
@@ -14,19 +14,53 @@ export default new CommandActionBuilder('Reaction.CardSearch_Small')
     .when(async (ctx) => !ctx.messageText.includes('[['))
     .do(async (ctx) => {
         for (const matchResult of ctx.matchResults) {
-            const response = await fetch(
-                `https://api.scryfall.com/cards/named?fuzzy=${matchResult[1]}`
+            let images: string[];
+            if (matchResult[1].includes('|')) {
+                let useBack = false;
+                const cardName = matchResult[1].split('|')[0];
+                let subquery = matchResult[1].split('|')[1];
+
+                if (subquery == 'flip') {
+                    useBack = true;
+                    subquery = '';
+                }
+                const response = await fetch(
+                    `https://api.scryfall.com/cards/search?q=${cardName} ${subquery}`
+                );
+                const data = await response.json();
+
+                if (data.status == 404) continue;
+
+                const firstMatch = data.data[0];
+
+                const cards: IScryfallCardInfo[] = firstMatch.card_faces
+                    ? firstMatch.card_faces
+                    : [firstMatch];
+                images = cards.map((x) =>
+                    getCardText(x, firstMatch.image_uris)
+                );
+
+                if (useBack) images.shift();
+            } else {
+                const response = await fetch(
+                    `https://api.scryfall.com/cards/named?fuzzy=${matchResult[1]}`
+                );
+                const data = await response.json();
+
+                if (data.status == 404) continue;
+
+                const cards: IScryfallCardInfo[] = data.card_faces
+                    ? data.card_faces
+                    : [data];
+                images = cards.map((x) => getCardText(x, data.image_uris));
+            }
+
+            ctx.replyWithText(
+                `[\\.](${
+                    images[0] ??
+                    'https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/f/f8/Magic_card_back.jpg'
+                })`
             );
-            const data = await response.json();
-
-            if (data.status == 404) continue;
-
-            const cards: IScryfallCardInfo[] = data.card_faces
-                ? data.card_faces
-                : [data];
-            const images = cards.map((x) => getCardText(x, data.image_uris));
-
-            ctx.replyWithText(`[\\.](${images[0]})`);
         }
     })
     .build();
