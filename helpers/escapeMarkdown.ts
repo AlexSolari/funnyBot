8c66a0402fc19d63c8949d2b6fee5f2462c82bf6
Alex Solari <alexsanify@gmail.com>
2025-01-26 19:06:07 +0200

    fixed telegram markdown escape

--- helpers/escapeMarkdown.ts
+++ helpers/escapeMarkdown.ts
@@ -6,5 +6,7 @@ export default function escapeMarkdown(text: string) {
         .replaceAll(/-/g, '\\-')
         .replaceAll(/\+/g, '\\+')
         .replaceAll(/\{/g, '\\{')
+        .replaceAll(/!/g, '\\!')
+        .replaceAll(/\|/g, '\\|')
         .replaceAll(/\}/g, '\\}');
 }
