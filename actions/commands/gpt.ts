145001f4eb4afc58f5f4f5cadfb2f46e4d6b947d
Alex Solari <alexsanify@gmail.com>
2025-07-09 10:44:05 +0300

    lib version bump & gpt ratelimit

--- actions/commands/gpt.ts
+++ actions/commands/gpt.ts
@@ -1,24 +1,30 @@
-import { CommandActionBuilder, MessageType, Seconds } from 'chz-telegram-bot';
+import { CommandActionBuilder, Hours, MessageType } from 'chz-telegram-bot';
 import OpenAI from 'openai';
 import escapeMarkdown from '../../helpers/escapeMarkdown';
 import { ChatId } from '../../types/chatIds';
 import openAiToken from '../../openAiToken.json';
 import { randomInt } from 'crypto';
+import { SpecificUsers } from '../../types/userIds';
+import { hoursToSeconds } from 'chz-telegram-bot/dist/helpers/timeConvertions';
 
+const whitelist = [SpecificUsers.nerdik, SpecificUsers.otabapa];
 const client = new OpenAI({
     apiKey: openAiToken.token
 });
 
 export default new CommandActionBuilder('Reaction.Gpt')
     .on(MessageType.Text)
-    .when((_) => randomInt(0, 20) == 0)
+    .when(
+        (ctx) => randomInt(0, 20) == 0 && !whitelist.includes(ctx.fromUserId!)
+    )
     .do(async (ctx) => {
         const response = await client.responses.create({
             model: 'gpt-4.1',
-            input: `Write a response to following message, be edgy and funny. If possible make an MTG reference relevant to the message contents. \n\n${ctx.messageText}`
+            input: `Write a response to following message, be edgy and funny. If possible make a MTG reference relevant to the message contents. \n\n${ctx.messageText}`
         });
         ctx.reply.withText(escapeMarkdown(response.output_text));
     })
-    .cooldown(30 as Seconds)
+    .ratelimit(1)
+    .cooldown(hoursToSeconds(1 as Hours))
     .ignoreChat(ChatId.PauperChat)
     .build();
