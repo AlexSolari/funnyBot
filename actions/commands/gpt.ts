cfbe9d9e7b56fb4255f8682e9bceaa98488b184f
Alex Solari <alexsanify@gmail.com>
2025-07-21 01:10:35 +0300

    added GPT capture "loop"

--- actions/commands/gpt.ts
+++ actions/commands/gpt.ts
@@ -1,8 +1,13 @@
 import {
+    ChatInfo,
     CommandActionBuilderWithState,
     Hours,
     hoursToSeconds,
-    MessageType
+    IActionState,
+    MessageType,
+    ReplyContext,
+    Seconds,
+    secondsToMilliseconds
 } from 'chz-telegram-bot';
 import OpenAI from 'openai';
 import escapeMarkdown from '../../helpers/escapeMarkdown';
@@ -16,7 +21,31 @@ const client = new OpenAI({
     apiKey: openAiToken.token
 });
 
-export default new CommandActionBuilderWithState(
+async function getReplyText(
+    text: string,
+    chatInfo: ChatInfo,
+    messageHistory?: string[]
+) {
+    const index = chatInfo.messageHistory.findIndex((x) => x.text == text);
+    const messagesBeforeTarget = chatInfo.messageHistory
+        .filter((_, i) => i <= index)
+        .map((x) => `${x.from?.username}: ${x.text}`);
+
+    const response = await client.responses.create({
+        model: 'gpt-4.1',
+        input: `Write a response to following message, be edgy and funny. 
+            If possible make a MTG reference relevant to the message contents, but be sure to not overdo it.
+            MTG reference should make sence in context of reply itself and chat history.
+            Reply language should be Ukraininan. Make sure that reply is short, 50 words max. 
+            Here's chat history before the message so you now have a context of a discussion:\n\n[${(
+                messageHistory ?? messagesBeforeTarget
+            ).join('\n')}]
+            Here's the message you need to reply:\n\n${text}`
+    });
+    return response;
+}
+
+export const gpt = new CommandActionBuilderWithState(
     'Reaction.Gpt',
     () => new GptState()
 )
@@ -29,24 +58,46 @@ export default new CommandActionBuilderWithState(
             ctx.userInfo.id != state.lastUserId
     )
     .do(async (ctx, state) => {
+        const conversation = [`${ctx.userInfo.name}: ${ctx.messageInfo.text}`];
         state.lastUserId = ctx.userInfo.id!;
-        const index = ctx.chatInfo.messageHistory.findIndex(
-            (x) => x.text == ctx.messageInfo.text
-        );
-        const messagesBeforeTarget = ctx.chatInfo.messageHistory
-            .filter((_, i) => i <= index)
-            .map((x) => `${x.from?.username}: ${x.text}\n`);
+        const response = await getReplyText(ctx.messageInfo.text, ctx.chatInfo);
 
-        const response = await client.responses.create({
-            model: 'gpt-4.1',
-            input: `Write a response to following message, be edgy and funny. 
-            If possible make a MTG reference relevant to the message contents, but be sure to not overdo it.
-            MTG reference should make sence in context of reply itself and chat history.
-            Reply language should be Ukraininan. Make sure that reply is short, 50 words max. 
-            Here's chat history before the message so you now have a context of a discussion:\n\n[${messagesBeforeTarget}]
-            Here's the message you need to reply:\n\n${ctx.messageInfo.text}`
-        });
-        ctx.reply.withText(escapeMarkdown(response.output_text));
+        conversation.push(`You: ${response.output_text}`);
+        const captureController = ctx.reply.withText(
+            escapeMarkdown(response.output_text)
+        );
+
+        const abortController = new AbortController();
+        const timer = setTimeout(
+            () => abortController.abort(),
+            secondsToMilliseconds(300 as Seconds)
+        );
+        const replyHandler = async (replyCtx: ReplyContext<IActionState>) => {
+            conversation.push(
+                `${replyCtx.userInfo.name}: ${replyCtx.messageInfo.text}`
+            );
+            const response = await getReplyText(
+                replyCtx.messageInfo.text,
+                replyCtx.chatInfo,
+                conversation
+            );
+            conversation.push(`You: ${response.output_text}`);
+
+            const captureController = replyCtx.reply.withText(
+                escapeMarkdown(response.output_text)
+            );
+            captureController.captureReplies(
+                [MessageType.Text],
+                replyHandler,
+                abortController
+            );
+            timer.refresh();
+        };
+        captureController.captureReplies(
+            [MessageType.Text],
+            replyHandler,
+            abortController
+        );
     })
     .withRatelimit(1)
     .withCooldown({ seconds: hoursToSeconds(20 as Hours) })
