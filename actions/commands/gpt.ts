00639bcbb3f81704c456ea1fa2f030656d52f470
Alex Solari <alexsanify@gmail.com>
2025-07-23 11:41:05 +0300

    improved gpt command

--- actions/commands/gpt.ts
+++ actions/commands/gpt.ts
@@ -25,6 +25,7 @@ const client = new OpenAI({
 async function getReplyText(
     text: string,
     chatInfo: ChatInfo,
+    promt: string,
     messageHistory?: string[]
 ) {
     const index = chatInfo.messageHistory.findIndex((x) => x.text == text);
@@ -34,10 +35,7 @@ async function getReplyText(
 
     const response = await client.responses.create({
         model: 'gpt-4.1',
-        input: `Write a response to following message, be edgy and funny. 
-            If possible make a MTG reference relevant to the message contents, but be sure to not overdo it.
-            MTG reference should make sence in context of reply itself and chat history.
-            Reply language should be Ukraininan. Make sure that reply is short, 50 words max. 
+        input: `${promt} 
             Here's chat history before the message so you now have a context of a discussion:\n\n[${(
                 messageHistory ?? messagesBeforeTarget
             ).join('\n')}]
@@ -62,7 +60,14 @@ export const gpt = new CommandActionBuilderWithState(
         state.lastUserId = ctx.userInfo.id!;
 
         const conversation = [`${ctx.userInfo.name}: ${ctx.messageInfo.text}`];
-        const response = await getReplyText(ctx.messageInfo.text, ctx.chatInfo);
+        const response = await getReplyText(
+            ctx.messageInfo.text,
+            ctx.chatInfo,
+            `Write a response to following message, be edgy and funny. 
+            If possible make a MTG reference relevant to the message contents, but be sure to not overdo it.
+            MTG reference should make sence in context of reply itself and chat history.
+            Reply language should be Ukraininan. Make sure that reply is short, 50 words max.`
+        );
 
         conversation.push(`You: ${response.output_text}`);
 
@@ -70,8 +75,14 @@ export const gpt = new CommandActionBuilderWithState(
             escapeMarkdown(response.output_text)
         );
 
+        if (
+            ctx.chatInfo.id == ChatId.LvivChat ||
+            ctx.chatInfo.id == ChatId.FrankivskChat
+        )
+            return;
+
         const { controller, timer } = getAbortControllerWithTimeout(
-            secondsToMilliseconds(300 as Seconds)
+            secondsToMilliseconds(120 as Seconds)
         );
         const replyHandler = async (replyCtx: ReplyContext<IActionState>) => {
             conversation.push(
@@ -80,6 +91,9 @@ export const gpt = new CommandActionBuilderWithState(
             const response = await getReplyText(
                 replyCtx.messageInfo.text,
                 replyCtx.chatInfo,
+                `Write a response to following message.
+                Make sure that reply makes sence in a context of a discussion.
+                Reply language should be Ukraininan. Make sure that reply is short, 50 words max.`,
                 conversation
             );
             conversation.push(`You: ${response.output_text}`);
@@ -94,6 +108,7 @@ export const gpt = new CommandActionBuilderWithState(
             );
             timer.refresh();
         };
+
         captureController.captureReplies(
             [MessageType.Text],
             replyHandler,
