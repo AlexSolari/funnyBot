6b081a87eea0fb674d6fe8960f0f976d6abac7bf
Alex Solari <alexsanify@gmail.com>
2025-08-19 10:08:46 +0300

    fixed becker api

--- actions/scheduled/becker.ts
+++ actions/scheduled/becker.ts
@@ -14,7 +14,8 @@ export const becker = new ScheduledActionBuilderWithState<BeckerState>(
     .do(async (ctx, _, state) => {
         const offset = randomInt(0, 26) * 50;
         const contentPage = await fetch(
-            `https://coomer.st/api/v1/onlyfans/user/alina_becker/posts?o=${offset}`
+            `https://coomer.st/api/v1/onlyfans/user/alina_becker/posts?o=${offset}`,
+            { headers: { Accept: 'text/css' } }
         );
         const data = (await contentPage.json()) as CmerApiResponse;
         const images = data.filter((x) => x.file.path.endsWith('.jpg'));
