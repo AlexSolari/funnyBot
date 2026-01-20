4545152b5e19e287c70cccc2733584744e9f3628
Alex Solari <alexsanify@gmail.com>
2026-01-20 17:56:54 +0200

    Updated slon command: added new gif variation

--- actions/commands/slon.ts
+++ actions/commands/slon.ts
@@ -1,8 +1,9 @@
 import { CommandBuilder } from '../../helpers/commandBuilder';
+import { randomInt } from '../../helpers/randomInt';
 
 export const slon = new CommandBuilder('Reaction.Slon')
     .on(/слон/i)
     .do(async (ctx) => {
-        ctx.reply.andQuote.withVideo('slon');
+        ctx.reply.andQuote.withVideo(`slon${randomInt(1, 2)}`);
     })
     .build();
