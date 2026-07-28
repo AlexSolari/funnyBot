80f924ce4ad67484492f01979fb251b4a3083ad7
Alex Solari <alexsanify@gmail.com>
2026-07-28 21:59:16 +0300

    Refactor error handling and improve readability in becker scheduled action

--- actions/scheduled/becker.ts
+++ actions/scheduled/becker.ts
@@ -1,5 +1,8 @@
 import { ChatId } from '../../types/chatIds';
-import { ScheduledActionBuilderWithState } from 'chz-telegram-bot';
+import {
+    BotEventType,
+    ScheduledActionBuilderWithState
+} from 'chz-telegram-bot';
 import BeckerState from '../../state/beckerState';
 import escapeMarkdown from '../../helpers/escapeMarkdown';
 import { randomInt } from '../../helpers/randomInt';
@@ -14,26 +17,42 @@ export const becker = new ScheduledActionBuilderWithState<BeckerState>(
     .runAt(11)
     .in([ChatId.GenshinChat])
     .do(async (ctx, _, state) => {
-        const offset = randomInt(0, 26) * 50;
-        const contentPage = await traceFetch(
-            `https://coomer.st/api/v1/onlyfans/user/alina_becker/posts?o=${offset}`,
-            getObservability(ctx),
-            { headers: { Accept: 'text/css' } }
-        );
-        const data = (await contentPage.json()) as CmerApiResponse;
-        const images = data.filter((x) => x.file.path.endsWith('.jpg'));
-        const imageContainer = images[randomInt(0, images.length)];
-
-        if (imageContainer.id != state.id) {
-            state.id = imageContainer.id;
-
-            ctx.send.text(
-                `[${escapeMarkdown(
-                    imageContainer.title
-                )}](https://img.coomer.st/thumbnail/data${
-                    imageContainer.file.path
-                })`
+        try {
+            const offset = randomInt(0, 26) * 50;
+            const contentPage = await traceFetch(
+                `https://coomer.st/api/v1/onlyfans/user/alina_becker/posts?o=${offset}`,
+                getObservability(ctx),
+                { headers: { Accept: 'text/css' } }
             );
+
+            if (!contentPage.ok) {
+                throw new Error(
+                    `Failed to fetch content page: ${contentPage.statusText}`
+                );
+            }
+
+            const data = (await contentPage.json()) as CmerApiResponse;
+            const images = data.filter((x) => x.file.path.endsWith('.jpg'));
+            const imageContainer = images[randomInt(0, images.length)];
+
+            if (imageContainer.id != state.id) {
+                state.id = imageContainer.id;
+
+                ctx.send.text(
+                    `[${escapeMarkdown(
+                        imageContainer.title
+                    )}](https://img.coomer.st/thumbnail/data${
+                        imageContainer.file.path
+                    })`
+                );
+            }
+        } catch (error) {
+            ctx.observability.eventEmitter.emit(BotEventType.error, {
+                error: error as Error,
+                traceId: ctx.observability.traceId
+            });
+
+            ctx.send.text('Беккеровозы в чат не приехали 😭');
         }
     })
     .build();
